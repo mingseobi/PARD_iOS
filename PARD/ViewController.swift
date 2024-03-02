aee48bac102a3f8e94223976e476f37f59e8cbbf
haram22 <kimhr9889@handong.ac.kr>
2024-03-02 12:43:31 +0900

    [Init] init commit

---
+++ PARD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PARD
+//
+//  Created by 김하람 on 3/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
