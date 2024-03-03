8925b75733250a081fa0e750f1031f948911b255
haram22 <kimhr9889@handong.ac.kr>
2024-03-03 19:05:50 +0900

    [Fix] 폴더구조 수정

--- PARD/ViewController.swift
+++ PARD/ViewController.swift
@@ -18,17 +18,49 @@ class ViewController: UIViewController {
         setUi()
     }
     
+    private lazy var normalButton = NormalButton(title: "normal Button", didTapHandler: normalButtonTapped).then{
+        view.addSubview($0)
+    }
+    
+    private lazy var changeNormalButton = NormalButton(title: "change normal button", didTapHandler: changeEnable).then{
+        view.addSubview($0)
+    }
+    
+    private lazy var titleLabel = UILabel().then{
+        view.addSubview($0)
+        $0.text = "< Test >"
+        $0.font = displayLarge
+        $0.textColor = .pard.primaryPurple
+    }
+    
     private func setUi() {
-        var titleLabel = UILabel().then{
-            view.addSubview($0)
-            $0.text = "< Test >"
-            $0.font = displayLarge
-            $0.textColor = .pard.primaryPurple
-            $0.snp.makeConstraints{ make in
-                make.centerX.equalToSuperview()
-                make.centerY.equalToSuperview()
-            }
+        titleLabel.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
         }
+        
+        normalButton.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.left.equalTo(30)
+            make.right.equalTo(-30)
+            make.top.equalTo(titleLabel.snp.bottom).offset(30)
+        }
+        
+        changeNormalButton.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.left.equalTo(30)
+            make.right.equalTo(-30)
+            make.top.equalTo(normalButton.snp.bottom).offset(30)
+            make.height.equalTo(48)
+        }
+    }
+    
+    @objc func normalButtonTapped() {
+        print("🌱 normal tapped !")
+    }
+    
+    @objc func changeEnable() {
+        normalButton.isEnabled.toggle()
     }
 }
 
