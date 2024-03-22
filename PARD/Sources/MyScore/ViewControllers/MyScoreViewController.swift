dc1e320e5b231ec81bc13a138f6b2498123d4f48
mingseobi <22200078@handong.ac.kr>
2024-03-22 16:49:43 +0900

    [init] myScoreViewController UI setting

--- PARD/Sources/MyScore/ViewControllers/MyScoreViewController.swift
+++ PARD/Sources/MyScore/ViewControllers/MyScoreViewController.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 class MyScoreViewController: UIViewController {
-
+    
     let pardnerShipLabel = UILabel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .pard.blackBackground
-
+        
         setNavigation()
         setupTextLabel()
         setupRankingMedals()
         setupRankingButton()
-        setupCrownImages() // 왕관 이미지 설정 추가
+        setupCrownImages()
+        setupScoreView()
     }
     
     // Navigation 설정
@@ -59,7 +60,6 @@ class MyScoreViewController: UIViewController {
         }
     }
     
-    // "전체랭킹 확인하기" 버튼 추가 및 액션 설정
     private func setupRankingButton() {
         let rankingButton = UIButton(type: .system).then {
             $0.setTitle("전체랭킹 확인하기", for: .normal)
@@ -70,14 +70,22 @@ class MyScoreViewController: UIViewController {
         view.addSubview(rankingButton)
         
         rankingButton.snp.makeConstraints {
-            $0.top.equalTo(pardnerShipLabel.snp.bottom).offset(171)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(200)
-            $0.height.equalTo(40)
+            $0.leading.equalToSuperview().offset(240)
+            $0.top.equalToSuperview().offset(325)
+            $0.width.equalTo(120)
+            $0.height.equalTo(14)
         }
+        
+        // Add underline using NSAttributedString
+        let attributedString = NSMutableAttributedString(string: "전체랭킹 확인하기", attributes: [
+            .font: UIFont.systemFont(ofSize: 12),
+            .foregroundColor: UIColor.pard.gray30
+        ])
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+        rankingButton.setAttributedTitle(attributedString, for: .normal)
     }
-    
-    // 메달 형식의 랭킹을 나타내는 함수
+
+
     private func setupRankingMedals() {
         // Gold Medal
         let goldRingImageView = UIImageView(image: UIImage(named: "goldRing"))
@@ -93,7 +101,7 @@ class MyScoreViewController: UIViewController {
         
         let goldPartLabel = UILabel().then {
             $0.text = "디자인파트"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.font = UIFont.systemFont(ofSize: 12)
             $0.textAlignment = .center
             $0.textColor = .pard.gray30
         }
@@ -101,9 +109,9 @@ class MyScoreViewController: UIViewController {
         
         let goldNameLabel = UILabel().then {
             $0.text = "김민섭"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.font = UIFont.systemFont(ofSize: 14)
             $0.textAlignment = .center
-            $0.textColor = .pard.gray30
+            $0.textColor = .pard.gray10
         }
         view.addSubview(goldNameLabel)
         
@@ -120,8 +128,8 @@ class MyScoreViewController: UIViewController {
         view.addSubview(silverRankLabel)
         
         let silverPartLabel = UILabel().then {
-            $0.text = "웹파트"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.text = "디자인파트"
+            $0.font = UIFont.systemFont(ofSize: 12)
             $0.textAlignment = .center
             $0.textColor = .pard.gray30
         }
@@ -129,9 +137,9 @@ class MyScoreViewController: UIViewController {
         
         let silverNameLabel = UILabel().then {
             $0.text = "손동우"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.font = UIFont.systemFont(ofSize: 14)
             $0.textAlignment = .center
-            $0.textColor = .pard.gray30
+            $0.textColor = .pard.gray10
         }
         view.addSubview(silverNameLabel)
         
@@ -148,8 +156,8 @@ class MyScoreViewController: UIViewController {
         view.addSubview(bronzeRankLabel)
         
         let bronzePartLabel = UILabel().then {
-            $0.text = "기획파트"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.text = "디자인파트"
+            $0.font = UIFont.systemFont(ofSize: 12)
             $0.textAlignment = .center
             $0.textColor = .pard.gray30
         }
@@ -157,9 +165,9 @@ class MyScoreViewController: UIViewController {
         
         let bronzeNameLabel = UILabel().then {
             $0.text = "윤성익"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.font = UIFont.systemFont(ofSize: 14)
             $0.textAlignment = .center
-            $0.textColor = .pard.gray30
+            $0.textColor = .pard.gray10
         }
         view.addSubview(bronzeNameLabel)
         
@@ -171,67 +179,68 @@ class MyScoreViewController: UIViewController {
             $0.leading.equalToSuperview().offset(22)
             $0.width.height.equalTo(40)
         }
-
+        
         goldRankLabel.snp.makeConstraints {
             $0.centerX.equalTo(goldRingImageView)
             $0.centerY.equalTo(goldRingImageView)
         }
-
+        
         goldPartLabel.snp.makeConstraints {
-            $0.leading.equalTo(goldRingImageView.snp.trailing).offset(8)
-            $0.centerY.equalTo(goldRingImageView)
+            $0.leading.equalToSuperview().offset(62)
+            $0.top.equalToSuperview().offset(179)
         }
 
+        
         goldNameLabel.snp.makeConstraints {
             $0.leading.equalTo(goldRingImageView.snp.trailing).offset(8)
-            $0.top.equalTo(goldPartLabel.snp.bottom).offset(4)
+            $0.top.equalTo(goldPartLabel.snp.bottom).offset(2)
         }
         
         // Silver Medal Constraints
         silverRingImageView.snp.makeConstraints {
             $0.centerY.equalTo(goldRingImageView)
             $0.leading.equalToSuperview().offset(138)
-                $0.width.height.equalTo(40)
-            }
-            
-            silverRankLabel.snp.makeConstraints {
-                $0.centerX.equalTo(silverRingImageView)
-                $0.centerY.equalTo(silverRingImageView)
-            }
-            
-            silverPartLabel.snp.makeConstraints {
-                $0.leading.equalTo(silverRingImageView.snp.trailing).offset(8)
-                $0.centerY.equalTo(silverRingImageView)
-            }
-            
-            silverNameLabel.snp.makeConstraints {
-                $0.leading.equalTo(silverRingImageView.snp.trailing).offset(8)
-                $0.top.equalTo(silverPartLabel.snp.bottom).offset(4)
-            }
-            
-            // Bronze Medal Constraints
-            bronzeRingImageView.snp.makeConstraints {
-                $0.centerY.equalTo(goldRingImageView)
-                $0.leading.equalToSuperview().offset(254)
-                $0.width.height.equalTo(40)
-            }
-            
-            bronzeRankLabel.snp.makeConstraints {
-                $0.centerX.equalTo(bronzeRingImageView)
-                $0.centerY.equalTo(bronzeRingImageView)
-            }
-            
-            bronzePartLabel.snp.makeConstraints {
-                $0.leading.equalTo(bronzeRingImageView.snp.trailing).offset(8)
-                $0.centerY.equalTo(bronzeRingImageView)
-            }
-            
-            bronzeNameLabel.snp.makeConstraints {
-                $0.leading.equalTo(bronzeRingImageView.snp.trailing).offset(8)
-                $0.top.equalTo(bronzePartLabel.snp.bottom).offset(4)
-            }
+            $0.width.height.equalTo(40)
         }
         
+        silverRankLabel.snp.makeConstraints {
+            $0.centerX.equalTo(silverRingImageView)
+            $0.centerY.equalTo(silverRingImageView)
+        }
+        
+        silverPartLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(178)
+            $0.top.equalToSuperview().offset(179)
+        }
+        
+        silverNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(silverRingImageView.snp.trailing).offset(8)
+            $0.top.equalTo(silverPartLabel.snp.bottom).offset(2)
+        }
+        
+        // Bronze Medal Constraints
+        bronzeRingImageView.snp.makeConstraints {
+            $0.centerY.equalTo(goldRingImageView)
+            $0.leading.equalToSuperview().offset(254)
+            $0.width.height.equalTo(40)
+        }
+        
+        bronzeRankLabel.snp.makeConstraints {
+            $0.centerX.equalTo(bronzeRingImageView)
+            $0.centerY.equalTo(bronzeRingImageView)
+        }
+        
+        bronzePartLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(294)
+            $0.top.equalToSuperview().offset(179)
+        }
+        
+        bronzeNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(bronzeRingImageView.snp.trailing).offset(8)
+            $0.top.equalTo(bronzePartLabel.snp.bottom).offset(2)
+        }
+    }
+    
     // Crown Images 설정
     private func setupCrownImages() {
         // Gold Crown
@@ -264,25 +273,116 @@ class MyScoreViewController: UIViewController {
             $0.width.height.equalTo(20)
         }
     }
-
+    
+    
+    private func setupScoreView() {
+        // Border View 추가
+        let myScoreBorderView = UIView().then {
+            $0.backgroundColor = UIColor(red: 42/255, green: 42/255, blue: 42/255, alpha: 1)
+            $0.layer.cornerRadius = 8
+            $0.layer.borderWidth = 1
+            $0.layer.borderColor = UIColor(patternImage: gradientImage()).cgColor
+        }
+        view.addSubview(myScoreBorderView)
         
-        @objc private func rankingButtonTapped() {
-            let rankingViewController = RankingViewController()
-            navigationController?.pushViewController(rankingViewController, animated: true)
+        myScoreBorderView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(245)
+            $0.leading.equalToSuperview().offset(24)
+            $0.width.equalTo(155.5)
+            $0.height.equalTo(68)
         }
         
-        // 그라데이션 이미지 생성
-        func gradientImage() -> UIImage {
-            let gradientLayer = CAGradientLayer().then {
-                $0.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
-                $0.colors = [UIColor(red: 82/255, green: 98/255, blue: 245/255, alpha: 1).cgColor, UIColor(red: 123/255, green: 63/255, blue: 239/255, alpha: 1).cgColor]
-                $0.startPoint = CGPoint(x: 0, y: 0)
-                $0.endPoint = CGPoint(x: 1, y: 1)
-            }
-            UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-            gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-            let image = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return image!
+        let totalScoreBorderView = UIView().then {
+            $0.backgroundColor = UIColor(red: 42/255, green: 42/255, blue: 42/255, alpha: 1)
+            $0.layer.cornerRadius = 8
+            $0.layer.borderWidth = 1
+            $0.layer.borderColor = UIColor(patternImage: gradientImage()).cgColor
+        }
+        view.addSubview(totalScoreBorderView)
+        
+        totalScoreBorderView.snp.makeConstraints {
+            $0.top.equalTo(myScoreBorderView.snp.top)
+            $0.leading.equalTo(myScoreBorderView.snp.trailing).offset(16)
+            $0.width.equalTo(155.5)
+            $0.height.equalTo(68)
+        }
+        
+        // myScoreLabel 추가
+        let myScoreLabel = UILabel().then {
+            $0.text = "파트 내 랭킹"
+            $0.font = UIFont.pardFont.body1
+            $0.textAlignment = .center
+            $0.textColor = .pard.gray10
+        }
+        view.addSubview(myScoreLabel)
+        
+        myScoreLabel.snp.makeConstraints {
+            $0.top.equalTo(myScoreBorderView.snp.top).offset(12)
+            $0.centerX.equalTo(myScoreBorderView.snp.centerX)
+        }
+        
+        // 파트 내 등수 표시 Label 추가
+        let myRankLabel = UILabel().then {
+            $0.text = "3위" // 등수를 실제로 표시할 값으로 수정해야 합니다.
+            $0.font = UIFont.pardFont.body2 // 등수에 맞는 폰트로 수정해야 합니다.
+            $0.textAlignment = .center
+            $0.textColor = .pard.gray10
+        }
+        view.addSubview(myRankLabel)
+        
+        myRankLabel.snp.makeConstraints {
+            $0.top.equalTo(myScoreLabel.snp.bottom).offset(8)
+            $0.centerX.equalTo(myScoreBorderView.snp.centerX)
+        }
+        
+        let totalScoreLabel = UILabel().then {
+            $0.text = "전체 랭킹"
+            $0.font = UIFont.pardFont.body1
+            $0.textAlignment = .center
+            $0.textColor = .pard.gray10
+        }
+        view.addSubview(totalScoreLabel)
+        
+        totalScoreLabel.snp.makeConstraints {
+            $0.top.equalTo(totalScoreBorderView.snp.top).offset(12)
+            $0.centerX.equalTo(totalScoreBorderView.snp.centerX)
+        }
+        
+        // 전체 등수 표시 Label 추가
+        let totalRankLabel = UILabel().then {
+            $0.text = "13위" // 등수를 실제로 표시할 값으로 수정해야 합니다.
+            $0.font = UIFont.pardFont.body2 // 등수에 맞는 폰트로 수정해야 합니다.
+            $0.textAlignment = .center
+            $0.textColor = .pard.gray10
+        }
+        view.addSubview(totalRankLabel)
+        
+        totalRankLabel.snp.makeConstraints {
+            $0.top.equalTo(totalScoreLabel.snp.bottom).offset(8)
+            $0.centerX.equalTo(totalScoreBorderView.snp.centerX)
         }
     }
+
+
+
+    
+    @objc private func rankingButtonTapped() {
+        let rankingViewController = RankingViewController()
+        navigationController?.pushViewController(rankingViewController, animated: true)
+    }
+    
+    // 그라데이션 이미지 생성
+    func gradientImage() -> UIImage {
+        let gradientLayer = CAGradientLayer().then {
+            $0.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+            $0.colors = [UIColor(red: 82/255, green: 98/255, blue: 245/255, alpha: 1).cgColor, UIColor(red: 123/255, green: 63/255, blue: 239/255, alpha: 1).cgColor]
+            $0.startPoint = CGPoint(x: 0, y: 0)
+            $0.endPoint = CGPoint(x: 1, y: 1)
+        }
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
