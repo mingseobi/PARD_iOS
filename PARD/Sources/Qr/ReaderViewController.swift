ffc3252352cecc4a1d52a6c73e6da9c71d391441
haram22 <kimhr9889@handong.ac.kr>
2024-03-23 22:54:01 +0900

    [Feat] QR GUI 입히기

--- PARD/Sources/Qr/ReaderViewController.swift
+++ PARD/Sources/Qr/ReaderViewController.swift
@@ -12,19 +12,20 @@ class ReaderViewController: UIViewController {
     
     var readerView: QrReaderView!
     var titleLabel = UILabel()
+    var outlineIcon = UIImageView()
+    var cancelButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black.withAlphaComponent(0.4)
+        view.backgroundColor = UIColor(red: 133/255.0, green: 133/255.0, blue: 133/255.0, alpha: 0.4)
         setAttribute()
         setupLayouts()
         readerView.start()
     }
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            // 다른 뷰 컨트롤러가 나타날 때 네비게이션 바를 숨김
-            self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        }
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         readerView.start()
@@ -38,12 +39,16 @@ class ReaderViewController: UIViewController {
     func setAttribute() {
         readerView = QrReaderView()
         readerView.delegate = self
-        readerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(readerView)
         titleLabel.text = "테두리 안에 출석 QR코드를 인식해주세요."
         titleLabel.font = UIFont.pardFont.head2
         titleLabel.textColor = .pard.blackBackground
         view.addSubview(titleLabel)
+        outlineIcon.image = UIImage(named: "qr_outline")
+        view.addSubview(outlineIcon)
+        cancelButton.setImage(UIImage(named: "cancelButton"), for: .normal)
+        cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        view.addSubview(cancelButton)
     }
     
     func setupLayouts() {
@@ -56,12 +61,27 @@ class ReaderViewController: UIViewController {
             make.bottom.equalTo(readerView.snp.top).offset(-28)
             make.centerX.equalToSuperview()
         }
+        outlineIcon.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        cancelButton.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(readerView.snp.bottom).offset(80)
+        }
+    }
+    
+    @objc func cancelButtonTapped() {
+        print("button Tapped")
+//        if let tabBarController = self.tabBarController {
+//            tabBarController.selectedIndex = 0
+//        }
     }
 }
 
 extension ReaderViewController: ReaderViewDelegate {
     func readerComplete(status: ReaderStatus) {
-
+        
         var title = ""
         var message = ""
         switch status {
@@ -71,7 +91,7 @@ extension ReaderViewController: ReaderViewDelegate {
                 message = "QR코드 or 바코드를 인식하지 못했습니다.\n다시 시도해주세요."
                 break
             }
-
+            
             title = "알림"
             message = "인식성공\n\(code)"
         case .fail:
@@ -81,17 +101,17 @@ extension ReaderViewController: ReaderViewDelegate {
             if isButtonTap {
                 title = "알림"
                 message = "바코드 읽기를 멈추었습니다."
-//                self.titleLabel.isSelected = readerView.isRunning
+                // self.titleLabel.isSelected = readerView.isRunning
             } else {
-//                self.titleLabel.isSelected = readerView.isRunning
+                // self.titleLabel.isSelected = readerView.isRunning
                 return
             }
         }
-
+        
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-
+        
         let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-
+        
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
     }
