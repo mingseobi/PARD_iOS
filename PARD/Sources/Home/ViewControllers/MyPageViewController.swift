fcc536aa7c1acae2cbd1f6d0f061e12fed73dcef
mingseobi <22200078@handong.ac.kr>
2024-06-26 22:48:10 +0900

    [Feat] MyPageViewController Setting

--- PARD/Sources/Home/ViewControllers/MyPageViewController.swift
+++ PARD/Sources/Home/ViewControllers/MyPageViewController.swift
@@ -12,6 +12,70 @@ class MyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupConstraints()
     }
+    
+    private func setupUI() {
+        view.addSubview(myPageLabel)
+        view.addSubview(infoView)
+        
+        view.addSubview(infoLabel)
+
+    }
+
+    private func setupConstraints() {
+        myPageLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        infoView.snp.makeConstraints { make in
+            make.width.equalTo(327)
+            make.height.equalTo(96)
+            make.top.equalTo(myPageLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(infoView.snp.top).offset(-8)
+            make.leading.equalTo(infoView.snp.leading)
+        }
+        
+        
+    }
+    
+    private let myPageLabel: UILabel = {
+        let myPageLabel = UILabel()
+        myPageLabel.text = "마이 페이지"
+        myPageLabel.textColor = .white
+        myPageLabel.font = UIFont.pardFont.head2
+        myPageLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        myPageLabel.textAlignment = .center
+        
+        return myPageLabel
+    }()
+    
+    private let infoView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor(red: 82/255, green: 98/255, blue: 245/255, alpha: 1).cgColor
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private let infoLabel: UILabel = {
+        let infolabel = UILabel()
+        infolabel.text = "내 정보"
+        infolabel.textColor = .white
+        infolabel.textAlignment = .left
+        infolabel.font = UIFont.pardFont.head1
+        infolabel.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        return infolabel
+    }()
+    
+    
+    
 }
